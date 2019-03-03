67c6eeec7daa1e646b1969f4cdc676794190d0ce
Dylan Thrush <dylanthrush1@gmail.com>
2019-03-03 13:45:47 -0800

    Got spectrogram working

--- music-to-color.py
+++ music-to-color.py
@@ -1,13 +1,12 @@
 import argparse
 import numpy as np
 import math
-from PIL import Image, ImageFilter
+from PIL import Image
 from pathlib import Path
 import soundfile as sf
 import cv2
 from matplotlib import cm
 import matplotlib.pyplot as plt
-from scipy import signal
 
 
 def get_arguments():
@@ -21,6 +20,19 @@ def get_arguments():
     parser.add_argument('-c', '--colormap', type=str, default='viridis', help='A matplotlib colormap name')
 
     return parser.parse_args()
+
+
+def get_data(file_path):
+    data, sample_rate = sf.read(file_path)
+
+    # just get one channel
+    if len(data) > 1:
+        data = data[:, 0]
+
+    # remove zeros, they mess with the spectrogram
+    data = [x for x in data if x != 0.0]
+
+    return data, sample_rate
 
 
 def generate_image(file_path, output_path, width, height, colormap):
@@ -43,7 +55,7 @@ def get_abs_samples(data, num_samples):
     samples = np.empty([0, 0])
 
     for i in range(num_samples):
-        samples = np.append(samples, data[i * decimation][0])
+        samples = np.append(samples, data[i * decimation])
 
     # shift up
     # samples = samples + abs(min(samples))
@@ -85,17 +97,15 @@ def get_colors(sound_samples, colormap):
     return color_list
 
 
-def draw_bars(data, width, height, colormap):
+def draw_bars(file_path, width, height, colormap):
+    data, sample_rate = get_data(file_path)
     samples = get_abs_samples(data, width)
     colors = get_colors(samples, colormap)
-
-    # plt.plot(samples)
-    # plt.show()
 
     im = Image.new('RGBA', (width, 1))
     im.putdata(colors)
     im = im.resize((width, height))
-    return im
+    im.save(str(output_path / file_path.parts[-1][:-4]) + ' ' + '(colorbars, ' + colormap + ')' + '.png')
 
 
 def draw_circles(data, width, height):
@@ -137,74 +147,115 @@ def draw_circles(data, width, height):
     return im
 
 
+def draw_spectrogram(file_path, width, height, colormap, dpi=300):
+    width = width/dpi
+    height = height/dpi
+
+    data, sample_rate = get_data(file_path)
+
+    fig = plt.figure(frameon=False, dpi=dpi)
+    fig.set_size_inches(width, height)
+    ax = plt.Axes(fig, [0., 0., 1., 1.])
+    ax.set_axis_off()
+    fig.add_axes(ax)
+
+    Pxx, freqs, bins, im = plt.specgram(data, Fs=sample_rate, NFFT=256, cmap=plt.get_cmap(colormap))
+
+    fig.savefig(str(output_path / file_path.parts[-1][:-4]) + ' ' + '(spectrogram, ' + colormap + ')' + '.jpeg')
+
+
 if __name__ == '__main__':
     args = get_arguments()
+    file_path = Path(args.file)
     output_path = Path('output')
+    audio_directory = Path('audio')
+
+    draw_bars(file_path, args.size[0], args.size[1], args.colormap)
+
+    # colormaps = ['viridis', 'plasma', 'inferno', 'magma']
+    #
+    # p = audio_directory.glob('**/*')
+    # files = [x for x in p if x.is_file()]
+    #
+    # for file in files:
+    #     print('Processing file: {}'.format(str(file)))
+    #     for color in colormaps:
+    #         print(color)
+    #         draw_bars(file, args.size[0], args.size[1], color)
+    #         draw_spectrogram(file, args.size[0], args.size[1], color)
+    #
+    #     print()
+
+    print()
+
     # generate_image(Path(args.file), output_path, args.size[0], args.size[1], args.colormap)
 
-    width = args.size[0]
-    height = args.size[1]
+    ### spectrogram ###
 
-    data, sample_rate = sf.read(Path(args.file))
+    # width = args.size[0]
+    # height = args.size[1]
+    #
+    # data, sample_rate = sf.read(Path(args.file))
+    #
+    # left_channel = data[:, 0]
+    #
+    # window_width = height*2  # sampling window
+    #
+    # sample_width = len(left_channel)//width
+    # windows = np.linspace(0, len(left_channel)-sample_width, num=width, dtype=int)
+    #
+    # color_map = get_colormap(args.colormap)
+    #
+    # i = 0
+    # pixels = []
+    #
+    # for window_start in windows:
+    #
+    #     samples = left_channel[window_start:window_start + window_width]  # Get chunk
+    #
+    #     sample_fft = np.fft.fft(samples)  # get fft
+    #     mag = np.abs(sample_fft)  # get magnitudes
+    #     mag = mag[0:height]  # only positives
+    #     # print(type(np.max(mag)))
+    #
+    #     max_mag = float(np.max(mag))
+    #
+    #     if max_mag != 0.0:
+    #         mag = mag * (255 / max_mag)  # scale
+    #
+    #     mag = mag.astype(int)  # cast as int
+    #
+    #     color_list = []
+    #     for sample in mag:
+    #         # print(sample)
+    #         color_list.append(color_map[int(sample)])
+    #
+    #     pixels.extend(color_list)
+    #     i += 1
+    #
+    # im = Image.new('RGBA', (height, width))
+    # print(len(pixels))
+    # print(width*height)
+    # im.putdata(pixels)
+    # im.save('spectrum.png')
 
-    left_channel = data[:, 0]
 
-    window_width = height*2  # sampling window
-
-    sample_width = len(left_channel)//width
-    windows = np.linspace(0, len(left_channel)-sample_width, num=width, dtype=int)
-
-    color_map = get_colormap(args.colormap)
-
-    i = 0
-    pixels = []
-
-    for window_start in windows:
-
-        samples = left_channel[window_start:window_start + window_width]  # Get chunk
-
-        sample_fft = np.fft.fft(samples)  # get fft
-        mag = np.abs(sample_fft)  # get magnitudes
-        mag = mag[0:height]  # only positives
-        # print(type(np.max(mag)))
-
-        max_mag = float(np.max(mag))
-
-        if max_mag != 0.0:
-            mag = mag * (255 / max_mag)  # scale
-
-        mag = mag.astype(int)  # cast as int
-
-        # freqs = np.fft.fftfreq(window, d=1/sample_rate)  # get frequencies
-        # freqs = freqs[0:height]  # only positives
-
-        color_list = []
-        for sample in mag:
-            # print(sample)
-            color_list.append(color_map[int(sample)])
-
-        pixels.extend(color_list)
-        i += 1
-
-    im = Image.new('RGBA', (height, width))
-    print(len(pixels))
-    print(width*height)
-    im.putdata(pixels)
-    im.save('spectrum.png')
-
-    # plt.plot(freqs, mag)
+    ### FFT ###
+    # width = args.size[0]
+    # height = args.size[1]
+    #
+    # data, sample_rate = sf.read(Path(args.file))
+    #
+    # start_time = 2*60 + 35.5  # start time in seconds
+    #
+    # start_sample = int(start_time*sample_rate)
+    #
+    # x = data[:, 0][start_sample:start_sample + 2**12]
+    # N = len(x)
+    # hx = np.fft.fft(x)
+    # f = np.fft.fftfreq(N, d=1/sample_rate)
+    # plt.plot(f[0:N//4], np.abs(hx[0:N//4]), linewidth=1.0)
+    # # plt.semilogx(f[0:N // 2], np.abs(hx[0:N // 2]), linewidth=1.0)
     # plt.show()
-    # print(len(mag))
-    # print(len(freqs))
-    # print('end')
 
     # matplotlib
-    # Pxx, freqs, bins, im = plt.specgram(data[:, 0], Fs=samplerate, NFFT=4096, cmap=plt.get_cmap('magma'))
-    # plt.show()
-
-    # scipy
-    # frequencies, times, spectrogram = signal.spectrogram(data[:, 0], samplerate)
-    # # plt.imshow(spectrogram)
-    # plt.ylabel('Frequency [Hz]')
-    # plt.xlabel('Time [sec]')
-    # plt.show()
